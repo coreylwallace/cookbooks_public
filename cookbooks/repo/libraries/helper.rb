4e3b99385c76f23a54b59f76a3e38af495ca28c0
NS <n.staknov@tut.by>
2012-01-16 22:24:44 +0300

    linknig git provider

--- cookbooks/repo/libraries/helper.rb
+++ cookbooks/repo/libraries/helper.rb
@@ -15,7 +15,7 @@ module RightScale
   def self.capistrano_pull(destination,repository,revision)
 
     shared_dir = Chef::Resource::Directory.new("#{destination.chomp}/shared/")
-    shared_dir.path = "#{destination.chomp}/shared/"
+   # shared_dir.path = "#{destination.chomp}/shared/"
     shared_dir.action_create
     #Chef::Directory "#{destination.chomp}/shared/" do
     #  recursive true
