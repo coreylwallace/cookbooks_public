6f762b9ef310a45f227217d2ad44780cfd3bdb14
NS <n.staknov@tut.by>
2012-01-17 00:22:23 +0300

    linknig git provider

--- cookbooks/repo/libraries/helper.rb
+++ cookbooks/repo/libraries/helper.rb
@@ -4,7 +4,7 @@
 # Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
- include Chef
+ include Chef::Provider
 
 module RightScale
   module Repo
