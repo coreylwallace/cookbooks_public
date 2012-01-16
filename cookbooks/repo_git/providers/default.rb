a51898e67102fd44fb2098eab5039fe653e793db
NS <n.staknov@tut.by>
2012-01-16 20:41:27 +0300

    linknig git provider

--- cookbooks/repo_git/providers/default.rb
+++ cookbooks/repo_git/providers/default.rb
@@ -23,6 +23,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+include RightScale::Repo::Helper
+
 action :pull do
  
   # add ssh key and exec script
