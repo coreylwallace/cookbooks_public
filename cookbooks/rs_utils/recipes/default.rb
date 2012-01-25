ea7b8d825c0458948cf1a1de50b6f561e4a441d2
unknown <cwallace@HOUCWALLACEPC2.PLANET.LOCAL>
2012-01-25 14:57:02 -0600

    Changed package install for ruby to not distinguish os's.
    Signed-off-by: unknown <cwallace@HOUCWALLACEPC2.PLANET.LOCAL>

--- cookbooks/rs_utils/recipes/default.rb
+++ cookbooks/rs_utils/recipes/default.rb
@@ -7,8 +7,10 @@
 
 rs_utils_marker :begin
 
-package "ruby" if node[:platform] == 'centos'
-package "ruby" if node[:platform] == 'ubuntu'
+package "ruby"
+
+# package "ruby" if node[:platform] == 'centos'
+# package "ruby" if node[:platform] == 'ubuntu'
 package "debian-helper-scripts" if node[:platform] == 'ubuntu' && node[:lsb][:codename] == 'hardy'
 
 include_recipe "rs_utils::setup_server_tags"
