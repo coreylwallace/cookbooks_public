917a6418cffaf9fee1c318c3869ec555c1028d44
unknown <cwallace@HOUCWALLACEPC2.PLANET.LOCAL>
2012-01-26 13:15:32 -0600

    Added cert update script

--- cookbooks/sys_firewall/recipes/default.rb
+++ cookbooks/sys_firewall/recipes/default.rb
@@ -9,6 +9,14 @@ rs_utils_marker :begin
 
 package "ruby"
 
+if node[:platform] == 'centos' || node[:platform] == 'redhat'
+  remote_file "/etc/pki/tls/certs/ca-bundle.crt" do
+    source "http://curl.haxx.se/ca/cacert.pem"
+    mode "0644"
+    #checksum "08da002l" # A SHA256 (or portion thereof) of the file.
+  end
+end
+
 if node[:sys_firewall][:enabled] == "enabled" 
   include_recipe "iptables"
   sys_firewall "22" # SSH
